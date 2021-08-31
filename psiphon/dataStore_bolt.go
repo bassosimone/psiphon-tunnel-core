035cf5caebfff345bf98b345ba9dddf8b3b1d696
Simone Basso <bassosimone@gmail.com>
2021-08-31 22:41:24 +0200

    chore: move github.com/Psiphon-Labs/bolt to internal

--- psiphon/dataStore_bolt.go
+++ psiphon/dataStore_bolt.go
@@ -29,7 +29,7 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/Psiphon-Labs/bolt"
+	"github.com/Psiphon-Labs/psiphon-tunnel-core/internal/github.com/Psiphon-Labs/bolt"
 	"github.com/Psiphon-Labs/psiphon-tunnel-core/psiphon/common/errors"
 )
 
